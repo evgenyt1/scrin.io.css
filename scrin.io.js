88217653394eeb00f76df9218f71f51029867240
Evgeny Tyukin <evgeny.tukin@gmail.com>
2021-04-28 19:54:25 +0300

    -

--- scrin.io.js
+++ scrin.io.js
@@ -1,5 +1,5 @@
 (function () {
-  
+  return;
   function preloadFonts() {
     /*
         <link rel="preload" href="https://scrin.io/Content/assets/fonts/EuclidCircularA-Regular.woff2" as="font" type="font/woff2" crossorigin>
