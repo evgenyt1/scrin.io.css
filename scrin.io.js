aaaa15a32546d6e59fe628927ec1e5df89a70a8e
Evgeny Tyukin <evgeny.tukin@gmail.com>
2021-04-29 17:31:34 +0300

    -

--- scrin.io.js
+++ scrin.io.js
@@ -1,46 +1,42 @@
 (function () {
-  function preloadFonts() {
-    /*
+    function preloadFonts() {
+        /*
         <link rel="preload" href="https://scrin.io/Content/assets/fonts/EuclidCircularA-Regular.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="https://scrin.io/Content/assets/fonts/EuclidCircularA-Italic.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="https://scrin.io/Content/assets/fonts/EuclidCircularA-Medium.woff2" as="font" type="font/woff2" crossorigin>
         <link rel="preload" href="https://scrin.io/Content/assets/fonts/EuclidCircularA-SemiBold.woff2" as="font" type="font/woff2" crossorigin>
         */
-  }
-
-  function shouldRun() {
-    if (
-      document.location.pathname !== "/" &&
-      document.location.pathname !== "/home" &&
-      document.location.pathname !== "/pricing"
-    )
-      return false;
-    if (
-      document.location.hostname !== "screenshotmonitor.com" &&
-      document.location.hostname !== "127.0.0.1"
-    )
-      return false;
-
-    const key = "barid2";
-    const force = document.location.search.indexOf("barforce=1") !== -1;
-    let myId = parseInt(localStorage.getItem(key));
-    if (!myId || force) {
-      myId = force ? 1 : Math.floor(Math.random() * 1000);
-      localStorage.setItem(key, myId);
     }
-    console.log("scrinio bar id", myId);
-    if (myId < 50) return true;
-    return false;
-  }
 
-  let alreadyRun = false;
-  function runScrinIo() {
-    if (!shouldRun()) return;
-    if (alreadyRun) return;
-    alreadyRun = true;
+    function shouldRun() {
+        if (
+            document.location.pathname !== "/" &&
+            document.location.pathname !== "/home" &&
+            document.location.pathname !== "/pricing"
+        )
+            return false;
+        if (document.location.hostname !== "screenshotmonitor.com" && document.location.hostname !== "127.0.0.1") return false;
 
-    // prepare
-    const html = `
+        const key = "barid2";
+        const force = document.location.search.indexOf("barforce=1") !== -1;
+        let myId = parseInt(localStorage.getItem(key));
+        if (!myId || force) {
+            myId = force ? 1 : Math.floor(Math.random() * 1000);
+            localStorage.setItem(key, myId);
+        }
+        console.log("scrinio bar id", myId);
+        if (myId < 50) return true;
+        return false;
+    }
+
+    let alreadyRun = false;
+    function runScrinIo() {
+        if (!shouldRun()) return;
+        if (alreadyRun) return;
+        alreadyRun = true;
+
+        // prepare
+        const html = `
   <div class="scrinio-bar unloaded">
     <div>
       <a href='https://scrin.io/?utm_source=ssm&utm_medium=bar&utm_campaign=rollout'>Hey there, try 
@@ -48,23 +44,20 @@
     </div>
   </div>
   `;
-    const div = document.createElement("div");
-    div.innerHTML = html;
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href =
-      document.location.hostname === "127.0.0.1"
-        ? "bar.css"
-        : "https://expofp.github.io/scrin.io.css/bar.css?1";
-    link.onload = function () {
-      document.body.prepend(div);
-      setTimeout(() => {
-        div.querySelector(".scrinio-bar").className = "scrinio-bar";
-      }, 1000);
-    };
-    document.head.appendChild(link);
-  }
+        const div = document.createElement("div");
+        div.innerHTML = html;
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = document.location.hostname === "127.0.0.1" ? "bar.css" : "https://expofp.github.io/scrin.io.css/bar.css?1";
+        link.onload = function () {
+            document.body.prepend(div);
+            setTimeout(() => {
+                div.querySelector(".scrinio-bar").className = "scrinio-bar";
+            }, 1000);
+        };
+        document.head.appendChild(link);
+    }
 
-  document.addEventListener("DOMContentLoaded", runScrinIo);
-  if (document.body) runScrinIo();
+    document.addEventListener("DOMContentLoaded", runScrinIo);
+    if (document.body) runScrinIo();
 })();
