e7bf47d05499d3ec42aecbf8d032292f6edd7335
Evgeny Tyukin <evgeny.tukin@gmail.com>
2021-05-11 09:42:24 +0300

    -

--- scrin.io.js
+++ scrin.io.js
@@ -27,7 +27,7 @@
             localStorage.setItem(key, myId);
         }
         console.log("scrinio bar id", myId);
-        if (myId < 100) return true;
+        if (myId < 500) return true;
         return false;
     }
 
